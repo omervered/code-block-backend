4733a061c35a55f7b22671e39f295b14fa050c6b
Omer Vered <omervered21@gmail.com>
2024-02-19 11:35:32 +0200

    adding settings to pkg json

--- app.js
+++ app.js
@@ -3,23 +3,24 @@ import path, { dirname } from "path";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import { loggerService } from "./services/logger.service.js";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-import { loggerService } from "./services/logger.service.js";
 
 const app = express();
 
 // Express App Config
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static("public"));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.resolve(__dirname, "public")));
   console.log("__dirname: ", __dirname);
 } else {
+  // Cors Config
   const corsOptions = {
+    // Making sure origin contains the url your frontend is running on
     origin: ["http://127.0.0.1:3000", "http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5173"],
     credentials: true,
   };
@@ -32,6 +33,7 @@ import { codeBlocksRoutes } from "./api/codeBlocks/codeBlocks.routes.js";
 app.use("/api/codeBlocks", codeBlocksRoutes);
 // app.use("/api/auth", authRoutes); if will have time to implement
 
+// Make every unmatched server-side-route fall back to index.html
 app.get("/**", (req, res) => {
   res.sendFile(path.resolve("public/index.html"));
 });
